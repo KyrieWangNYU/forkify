003133045325611dde1adf77e9b3eb3868da9405
KyrieWangNYU <kw2116@nyu.edu>
2020-04-21 12:00:22 -0700

    building search controller & building seasrch view first time

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,35 @@
 // Global app controller
+import Search from './models/Search';
+import {elements} from "./views/base";
+import * as searchView from './views/searchView';
 
-import num from './test';
+const state = {};
 
-const x = 23;
+const controlSearch = async () => {
+    //1.get the query from view
+    const query = searchView.getInput();
 
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+    if (query){
+        //2. new search object and add to state
+        state.search = new Search(query);
+
+        //3.Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        //4.Search for recipes
+        await state.search.getResults();
+
+        //5.render the result on UI
+        searchView.renderResults(state.search.result);
+
+    }
+
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
